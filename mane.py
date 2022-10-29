7858413c5ee8fbd41c81352d1454317470d66017
starlexleviaphan <vadimfustie1995@gmail.com>
2022-10-30 02:02:45 +0300

    commit 5: added function test_function() to test branch

--- mane.py
+++ mane.py
@@ -1,4 +1,4 @@
-def mane_function():
+def mane_function(a):
     if a == a[::-1]: return True
     sum = 0
     a = str(a)
@@ -9,3 +9,11 @@ def mane_function():
             for i in a:
         sum += int(i)
     return sum
+def test_function(a):
+    if a == a[::-1]: return True
+    sum = 0
+    a = str(a)
+    for i in a:
+        sum += int(i)
+    return sum
+    a = str(a)
