f9bcce8fe4248600159a068db7376ebcc4ca9e12
starlexleviaphan <vadimfustie1995@gmail.com>
2022-10-30 02:31:17 +0300

    added   git_hub file

---
+++ git_hub.py
@@ -0,0 +1,11 @@
+def git_hub():
+    if a == a[::-1]: return True
+    sum = 0
+    a = str(a)
+    for i in a:
+        sum += int(i)
+    a = str(a)
+    return sum
+    for i in a:
+        sum += int(i)
+    return sum
